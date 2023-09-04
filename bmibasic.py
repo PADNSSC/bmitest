16a150b41975678342d8de1a5797ad55a4e7e411
PADNSSC <141046240+PADNSSC@users.noreply.github.com>
2023-09-04 21:25:25 +0700

    Add files via upload

---
+++ bmibasic.py
@@ -0,0 +1,10 @@
+name = input ('what your name')
+cm = float(input('What is your height '))
+kg = float(input('What is your weight '))
+m = (cm*10)
+m2 = (m**2)
+BMI = float(kg/m2)
+print (name)
+print ('bmi' ,'of' ,name , BMI)
+
+#หา bmi โดยการ น้ำหนักตัว [กิโลกรัม] ÷ ส่วนสูง [เมตร] ยกกำลังสอง
